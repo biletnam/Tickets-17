9709ce7c02c247d11646a5c137b65704e28173f2
Abdiel Soto <abdielsba@gmail.com>
2016-03-18 00:42:13 -0600

    Initial Commit

---
+++ Tickets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tickets
+//
+//  Created by Abdiel Soto Barrera on 3/18/16.
+//  Copyright © 2016 Abdiel Soto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
