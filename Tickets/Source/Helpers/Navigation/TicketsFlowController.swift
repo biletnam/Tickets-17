6e196ad4bba448cc09dcd1a5e610389d9e5cc83d
Abdiel Soto <abdielsba@gmail.com>
2016-04-02 23:45:26 -0600

    Added TicketsFlowController

---
+++ Tickets/Source/Helpers/Navigation/TicketsFlowController.swift
@@ -0,0 +1,13 @@
+//
+//  TicketsFlowController.swift
+//  Tickets
+//
+//  Created by Abdiel Soto Barrera on 3/31/16.
+//  Copyright © 2016 SmartDev. All rights reserved.
+//
+
+import UIKit
+
+class TicketsFlowController: NSObject {
+
+}
